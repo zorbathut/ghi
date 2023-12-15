674bed604be2d12febd62267c7c255e37f42ee6b
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2023-12-15 10:27:23 -0600

    Remove some dead code.

--- src/Util.cs
+++ src/Util.cs
@@ -9,27 +9,6 @@ namespace Ghi
 
     internal static class Util
     {
-        internal static V TryGetValue<T, V>(this Dictionary<T, V> dict, T key)
-        {
-            dict.TryGetValue(key, out V holder);
-            return holder;
-        }
-
-        internal static V TryGetValue<T, V>(this Dictionary<T, V> dict, T key, V def)
-        {
-            if (dict.TryGetValue(key, out V holder))
-            {
-                return holder;
-            }
-
-            return def;
-        }
-
-        internal static Dictionary<K, V> ToDictionary<K, V>(this IEnumerable<Tuple<K, V>> enumerable)
-        {
-            return enumerable.ToDictionary(kvp => kvp.Item1, kvp => kvp.Item2);
-        }
-
         internal static int FirstIndexOf<T>(this IEnumerable<T> enumerable, Func<T, bool> func)
         {
             int index = 0;
